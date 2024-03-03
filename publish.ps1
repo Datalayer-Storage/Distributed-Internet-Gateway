bf8e81f6ea29bd8467407ddbeb36de4b6c6a0bcb
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-03-03 12:21:41 -0500

    ci: build debs

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.2.17"
+$version = "0.2.18"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
