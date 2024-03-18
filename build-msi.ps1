e53f17963a47aac6ee362e4ccceeecb730e02016
Don Kackman <dkackman@gmail.com>
2024-03-17 19:04:58 -0500

    dont publish

--- build-msi.ps1
+++ build-msi.ps1
@@ -4,7 +4,7 @@ $outputRoot = "./publish"
 
 # build the msi - win-x64 only
 if ([System.Environment]::OSVersion.Platform -eq "Win32NT") {
-    & ./publish.ps1 win-x64
+    # & ./publish.ps1 win-x64
 
     dotnet build ./$src/Installer/Windows/MsiInstaller.wixproj -c Release -r win-x64 --output $outputRoot
     Move-Item -Path $outputRoot/en-us/Distributed-Internet-Gateway.msi -Destination $outputRoot/$name-win-x64.msi
