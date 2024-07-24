2d6010f70283d1d4aa3d63f3a9330e915bf4db50
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-07-24 19:25:27 -0400

    fix: headers

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.2.41"
+$version = "0.2.42"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
