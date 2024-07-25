414c7dc19f37b3c80834e887bb081ee0a4b23af7
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-07-25 10:54:32 -0400

    fix: spa support

--- src/server/Controllers/StoresController.cs
+++ src/server/Controllers/StoresController.cs
@@ -28,10 +28,6 @@ public partial class StoresController(GatewayService gatewayService,
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<string>))]
     public async Task<IActionResult> GetStore(string storeId, CancellationToken cancellationToken)
     {
-        if (storeId is null || storeId.Length != 64)
-        {
-            return NotFound();
-        }
 
         try
         {
