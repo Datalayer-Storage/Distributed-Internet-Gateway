f3076ca0d52796c06fb3051bebdbf67ab6d660c1
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-07-31 21:03:11 -0400

    fix: wallet generation

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.2.64"
+$version = "0.2.65"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
