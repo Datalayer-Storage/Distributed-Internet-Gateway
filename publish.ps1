781c67db62c0de24d3b783e110181d3ebd506d29
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-08-04 12:19:48 -0400

    feat: mechanism to view historical roots

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.2.68"
+$version = "0.3.0"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
