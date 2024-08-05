4d32c409571aa1537c22cd0ba3a4944218779095
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-08-05 12:53:57 -0400

    fix: head logging

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.4.3"
+$version = "0.4.4"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
