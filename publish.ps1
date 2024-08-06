e0511efcc8724be58cda1216ef9ba6c1f0068a42
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-08-06 12:28:40 -0400

    feat: redirect management improvements

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.4.22"
+$version = "0.4.23"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
