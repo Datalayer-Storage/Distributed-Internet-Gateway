a3732ba2dc3849d1502e8d0632039c291199f063
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-08-06 19:15:52 -0400

    fix: redirect logic

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ param(
 )
 # other runtimes that might work: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 
-$version = "0.4.25"
+$version = "0.4.26"
 $fullName = "dig"
 $names = @("dig", "server")
 $src = "src"
